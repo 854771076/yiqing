452f17de61bcc553576f15e13fe3123c2f96598c
fuyang <854771076@qq.com>
2022-04-28 00:28:05 +0800

    新闻

--- yiqing/application/views.py
+++ yiqing/application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 import requests
+import time
 import json
 from application.models import Hero
 # Create your views here.
@@ -7,7 +8,7 @@ def index(request):
     news=requests.get('https://lab.isaaclin.cn/nCoV/api/news?page=1&num=10')
     news.encodingcoding=news.apparent_encoding
     news=json.loads(news.text)['results']
-    f=open(r'C:\Users\awsless\Desktop\疫情\yiqing\static\data\knowledge.json','r',encoding='utf8')
+    f=open(r'static\data\knowledge.json','r',encoding='utf8')
     knows=json.loads(f.read())['data']
     f.close()
     heros=Hero.objects.all()
@@ -15,4 +16,27 @@ def index(request):
         
     return render(request, 'index.html',content)
 def news(request):
-    return render(request, 'news.html')
+    if request.GET:
+        page=request.GET.get('page')
+        # status=request.GET.get('status')
+        # if status=='pre':
+        #     if int(page)==1:
+        #         pass
+        #     else:
+        #         page=int(page)-1
+        # elif status=='next':
+        #     page=int(page)+1
+        page=int(page)
+
+    else:
+        page=1
+    
+    news=requests.get(f'https://lab.isaaclin.cn/nCoV/api/news?page={page}&num=40')
+    news.encodingcoding=news.apparent_encoding
+    news=json.loads(news.text)['results']
+    for i in news:
+        # i['pubDate']=time.strptime('%Y-%m-%d %H:%M:%S',time.localtime(int(i['pubDate'])/1000))
+        times=time.localtime(int(i['pubDate'])/1000)
+        i['pubDate']=time.strftime('%Y-%m-%d %H:%M:%S',times)
+    content={'news':news,'page':int(page)}
+    return render(request, 'news.html',content)
